2d537b52d168e2179c1ea7aebc55fce26f419e7e
RakhatAubakirov <rakha.aubakirov@gmail.com>
2020-12-20 23:12:05 +0600

    hw-1

---
+++ HW1/js/main.js
@@ -0,0 +1,27 @@
+function drawSmiley(){
+    var canvas = document.getElementById('smiley');
+
+    if (canvas.getContext){
+       var ctx = canvas.getContext('2d');
+
+       
+       ctx.beginPath();
+       ctx.arc(75,75,50,0,Math.PI*2,true);  
+       
+       ctx.moveTo(110,75);
+       ctx.arc(75,75,35,0,Math.PI,false);   
+       
+       ctx.moveTo(65,65);
+       ctx.arc(60,65,5,0,Math.PI*2,true);  
+       
+       ctx.moveTo(95,65);
+       ctx.arc(90,65,5,0,Math.PI*2,true);  
+       
+       ctx.stroke();
+    }
+    else {
+       alert('Your browser does not support canvas.');
+    }
+ }
+
+ 
