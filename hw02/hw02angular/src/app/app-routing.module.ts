ac2cb75386d08a52081ab33f8cec69c39e9dcf7c
Nursultan Altayev <whiplahedy@gmail.com>
2020-12-27 23:13:58 +0600

    hw02angular

---
+++ hw02/hw02angular/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './pages/login/login.component';
+import {RegisterComponent} from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+
+
+const routes: Routes = [
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'home', component: HomeComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ hw02/hw02angular/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firebaseAuth';
+  constructor(public authservice: AuthService, private router: Router) { }
+}
