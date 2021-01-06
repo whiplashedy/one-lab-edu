acfe0adced0a4223a98b7caaa3f2af3c6ecd891d
Arystan Kalimov <arystan0kalimov@gmail.com>
2021-01-07 05:19:39 +0600

    Lesson5: in-class work

---
+++ Lesson5/Online/some-app/src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import { increment, decrement, reset } from '../counter.actions';
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css']
+})
+export class MyCounterComponent implements OnInit {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+
+  ngOnInit(): void { }
+
+  increment(): void {
+    this.store.dispatch(increment());
+  }
+
+  decrement(): void {
+    this.store.dispatch(decrement());
+  }
+
+  reset(): void {
+    this.store.dispatch(reset());
+  }
+}
